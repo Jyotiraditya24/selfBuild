c24ae6ba2b5f39288fa487d92bee4b472664ed3e
jyotiraditya <jyotiraditya24@gmail.com>
2024-01-23 22:08:56 +0530

    Basic connection Done

--- server/app.js
+++ server/app.js
@@ -1,13 +1,31 @@
 import express from "express";
+import { Server } from "socket.io";
+import { createServer } from "http";
+import cors from "cors";
 
-const port = 3001
+const port = 3001;
 
 const app = express();
+const server = createServer(app);
 
-app.listen(port,()=>{
-    console.log(`server running on ${port}`)
-})
+const io = new Server(server, {
+  cors: { 
+    origin:"http://localhost:5173",
+    methods:["GET","POST"],
+    credentials: true
+  }
+});
+app.use(cors()); //using cors as a middleware
 
-app.get("/",(req,resp)=>{
-    resp.send("Hello");
-})
+app.get("/", (req, resp) => {
+  resp.send("Hello");
+});
+
+io.on("connection", (socket) => {
+  console.log("connected", socket.id);
+});
+
+server.listen(port, () => {
+  console.log(`server running on ${port}`);
+});
+
