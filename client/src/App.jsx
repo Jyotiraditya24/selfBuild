d96bc2d3325778213d858eda08b4f77655353a9b
jyotiraditya <jyotiraditya24@gmail.com>
2024-01-26 23:44:13 +0530

    useMemo Applied

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,22 @@
 import React, { useEffect } from "react";
 import { io } from "socket.io-client";
-import { Container, Typography } from "@mui/material";
+import { Container, TextField, Typography, Button } from "@mui/material";
+import { useState } from "react";
 
 const App = () => {
   // give the backend server url
-  const socket = io("http://localhost:3001");
+  // io("http://localhost:3001")
+  const socket = useMemo(()=>io("http://localhost:3001"),[]);
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    socket.emit("message", message);
+    setMessage("");
+  };
+
+  const [message, setMessage] = useState("");
+  console.log("Hello");
+
   useEffect(() => {
     socket.on("connect", () => {
       console.log("connected", socket.id);
@@ -12,12 +24,30 @@ const App = () => {
     socket.on("Welcome", (s) => {
       console.log(s);
     });
+    console.log("Hello this is useEffect");
+    return () => {
+      socket.disconnect();
+    };
   }, []);
+
   return (
     <Container maxWidth="sm">
-      <Typography variant="h1" component="div" gutterBottom>
+      <Typography value={message} variant="h1" component="div" gutterBottom>
         Welcome to Socket.Io
       </Typography>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          id="outlined-basic"
+          label="Outlined"
+          variant="outlined"
+          onChange={(e) => 
+            setMessage(e.target.value)
+          }
+        />
+        <Button type="submit" variant="contained" color="primary">
+          Send
+        </Button>
+      </form>
     </Container>
   );
 };
