895e3cd2efb85cd9fac703e1aa725378d07d7aaa
jyotiraditya <jyotiraditya24@gmail.com>
2024-02-03 17:37:23 +0530

    Loader added & dynamic imports

--- ecommerce/client/src/App.tsx
+++ ecommerce/client/src/App.tsx
@@ -1,7 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { lazy, Suspense } from "react";
+import { Loader } from "./components/loader";
+
+const Home = lazy(() => import("./pages/home"));
+const Search = lazy(() => import("./pages/Search"));
+const Cart = lazy(() => import("./pages/Cart"));
+
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <Router>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </Suspense>
+    </Router>
+  );
+};
 
-export default App
+export default App;
